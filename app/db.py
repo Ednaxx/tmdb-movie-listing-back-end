2d4c590c9a67a66386ff4d56cc518df325ea6b15
Alexandre Morais <xandre2setembro2002@gmail.com>
2025-10-29 16:01:28 +0000

    feat: improve dev postgres setup

--- app/db.py
+++ app/db.py
@@ -2,6 +2,7 @@ from typing import Annotated
 from sqlmodel import Session, SQLModel, create_engine
 from fastapi import Depends
 from functools import lru_cache
+from sqlalchemy_utils import database_exists, create_database
 
 from .config import Settings
 
@@ -16,7 +17,12 @@ def get_engine():
     settings = get_settings()
     connect_args = {}
 
-    return create_engine(settings.database_url, connect_args=connect_args, echo=True)
+    engine = create_engine(settings.database_url, connect_args=connect_args, echo=True)
+
+    if not database_exists(engine.url):
+        create_database(engine.url)
+
+    return engine
 
 
 def create_db_and_tables():
